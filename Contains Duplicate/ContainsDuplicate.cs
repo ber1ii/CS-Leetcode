5c520b3e73e5d9375fd2459653c007b292cae2cc
ber1ii <vujkovicgh@gmail.com>
2025-09-29 23:00:14 +0200

    Contains Duplicate

---
+++ Contains Duplicate/ContainsDuplicate.cs
@@ -0,0 +1,18 @@
+public class ContainsDuplicate
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> seen = new HashSet<int>();
+        foreach (int num in nums)
+        {
+            if (seen.Contains(num))
+            {
+                return true;
+            }
+
+            seen.Add(num);
+        }
+
+        return false;
+    }
+}
