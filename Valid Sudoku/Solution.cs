ba488aa589896f8fe22af31d2618c7cd5aa6dcf6
ber1ii <vujkovicgh@gmail.com>
2025-10-01 20:07:10 +0200

    Valid Sudoku

---
+++ Valid Sudoku/Solution.cs
@@ -0,0 +1,42 @@
+public class Solution
+{
+    public bool ValidSudoku(char[][] board)
+    {
+        HashSet<char>[] rows = new HashSet<char>[9];
+        HashSet<char>[] cols = new HashSet<char>[9];
+        HashSet<char>[] boxes = new HashSet<char>[9];
+
+        for (int i = 0; i < 9; i++)
+        {
+            rows[i] = new HashSet<char>();
+            cols[i] = new HashSet<char>();
+            boxes[i] = new HashSet<char>();
+        }
+
+        for (int r = 0; r < 9; r++)
+        {
+            for (int c = 0; c < 9; c++)
+            {
+                char val = board[r][c];
+
+                if (val == '.') continue;
+
+                //Calculate boxIdx each cell belongs to
+                int boxIdx = (r / 3) * 3 + (c / 3);
+
+                //Check if row, column or current box contains the value we are
+                //trying to add into the current cell
+                if (rows[r].Contains(val) || cols[c].Contains(val) || boxes[boxIdx].Contains(val))
+                {
+                    return false;
+                }
+
+                rows[r].Add(val);
+                cols[c].Add(val);
+                boxes[boxIdx].Add(val);
+            }
+        }
+
+        return true;
+    }
+}
